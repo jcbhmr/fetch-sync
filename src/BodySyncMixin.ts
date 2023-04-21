f2c9d32938b5fbbe69c44ce1cc9c58e4573242c9
Jacob Hummer <jcbhmr@outlook.com>
2023-04-20 21:01:56 -0500

    dump

--- src/BodySyncMixin.ts
+++ src/BodySyncMixin.ts
@@ -1,23 +1,44 @@
 import MIMEType from "whatwg-mimetype";
 
-const body = new WeakMap<typeof BodySyncMixin.prototype, null | Uint8Array>();
-const mimeType = new WeakMap<typeof BodySyncMixin, () => MIMEType>();
+/**
+ * `.body` is now a `Uint8Array` instead. Normally, `.body` is a `ReadableStream<Uint8Array>`, but since we are in synchronous land, that won't cut it. The next-closest thing is either an `ArrayBuffer` or a raw complete `Uint8Array`. In this case, we have chosen the `Uint8Array` option since it fits with the `Uint8Array` from the `ReadableStream`.
+ */
+const body = new WeakMap<BodySyncMixin, null | Uint8Array>();
+const bodyUsed = new WeakMap<BodySyncMixin, boolean>();
+const mimeType = new WeakMap<BodySyncMixin, () => MIMEType>();
 
+type BodySyncMixin = typeof BodySyncMixin.prototype;
 const BodySyncMixin = {
   prototype: {
+    /**
+     * `.body` is now a `Uint8Array` instead. Normally, `.body` is a `ReadableStream<Uint8Array>`, but since we are in synchronous land, that won't cut it. The next-closest thing is either an `ArrayBuffer` or a raw complete `Uint8Array`. In this case, we have chosen the `Uint8Array` option since it fits with the `Uint8Array` from the `ReadableStream`.
+     */
     get body(): Uint8Array | null {
       return body.get(this)!;
     },
-    get bodyUsed(): false {
-      return false;
+
+    get bodyUsed(): boolean {
+      return bodyUsed.get(this)!;
     },
+
+    /**
+     * Consumes the entire body as an `ArrayBuffer`.
+     *
+     * @returns An `ArrayBuffer`, possibly with a length of zero
+     */
     arrayBuffer(): ArrayBuffer {
+      bodyUsed.set(this, true);
       if (this.body) {
         return this.body.buffer;
       } else {
         return new ArrayBuffer();
       }
     },
+
+    /**
+     * Consumes the body as a string.
+     * @returns A string, possibly empty
+     */
     text(): string {
       if (this.body) {
         return new TextDecoder().decode(this.body);
@@ -25,13 +46,24 @@ const BodySyncMixin = {
         return "";
       }
     },
-    blob(): any {
+
+    /**
+     * Transfers the entire body into a `Blob` instance.
+     * @returns A Blob, possibly with a size of zero
+     */
+    blob(): Blob {
       const type = mimeType.get(Object.getPrototypeOf(this))!.call(this);
       return new Blob([this.body], { type });
     },
+
+    /**
+     * Assumes the body is encoded as UTF-8. Parses the body as JSON and consumes it.
+     * @returns A string, number, boolean, object, array, or null JSON value.
+     */
     json(): any {
       return JSON.parse(this.text());
     },
+
     // TODO: Add .formData()
     // formData(): FormData {}
   },
--- src/fetchSync.ts
+++ src/fetchSync.ts
@@ -1,9 +1,18 @@
-function fetchSync(): ResponseSync {
-  let error: Error | null = null;
-  let returnValue: ResponseSync | null = null;
-
+/**
+ * Performs a **synchronous** network fetch request using `XMLHttpRequest` as
+ * a synchronous backend.
+ *
+ * ⚠️ This is **not recommended** to be used on the main
+ * thread, especially in a realtime browser application!
+ *
+ * @see https://fetch.spec.whatwg.org/#dom-global-fetch
+ */
+function fetchSync(
+  input: RequestSyncInfo,
+  init: RequestSyncInit = {}
+): ResponseSync {
   const xhr = new XMLHttpRequest();
-
+  xhr.open(requestSync.method, requestSync.url, false);
   if (requestSync.credentials === "include") {
     xhr.withCredentials = true;
   }
@@ -11,25 +20,24 @@ function fetchSync(): ResponseSync {
     xhr.setRequestHeader(name, value);
   }
   try {
+    // Note: You cannot change the value of responseType in a synchronous XMLHttpRequest except when the request belongs to a Worker. This restriction is designed in part to help ensure that synchronous operations aren't used for large transactions that block the browser's main thread, thereby bogging down the user experience.
     xhr.responseType = "arraybuffer";
   } catch {}
 
-  xhr.open(requestSync.method, requestSync.url, false);
   xhr.send(requestSync.body);
 
   const headers = xhr
     .getAllResponseHeaders()
-    .trim()
-    .split(/(?:\r?\n)+/)
+    .split(/\r?\n/)
+    .filter((line) => line.trim())
     .map((line) => line.split(": ", 2));
-
   const responseSync = new ResponseSync(xhr.body, {
     status: xhr.status,
     statusText: xhr.statusText,
-    headers: headers,
+    headers,
   });
   url.set(responseSync, xhr.responseURL);
-  redirected.set(responseSync, xhr.responseURL !== request.url);
+  redirected.set(responseSync, xhr.responseURL !== requestSync.url);
 
   return responseSync;
 }
